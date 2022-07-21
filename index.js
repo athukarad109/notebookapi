972df41dd09509249356fa5b6d24f9096a46a868
Atahrv <athukarad109@gmail.com>
2022-07-21 16:19:38 +0530

    Setting up app

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const connectToMongo = require('./db');
+
+const app = express();
+const port = 5000;
+
+connectToMongo();
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+})
+
+
+app.listen(port, () => {
+    console.log("Up and running");
+})
