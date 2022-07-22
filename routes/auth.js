9c9ba1e7ab124cb12061cad72d9c8571c4193234
Atahrv <athukarad109@gmail.com>
2022-07-22 15:15:14 +0530

    Addong validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 
 const router = express.Router();
 
 const User = require('../models/User');
 
 //creating user
-router.post('/', (req, res) => {
-    const user = User(req.body);
-    user.save();
-    res.send(user);
-    console.log("Success")
+router.post('/', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('name', 'Name cannot be this short').isLength({ min: 3 }),
+    body('password', 'Password cannot be this short').isLength({ min: 5 })
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        }).then(user => res.json(user))
+        .catch(e => res.json({ Error: e }))
+
 })
 
 module.exports = router
