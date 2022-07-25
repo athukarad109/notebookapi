12634e50ecf7519b943f01bbaf4d48a970998155
Atahrv <athukarad109@gmail.com>
2022-07-25 18:13:43 +0530

    Adding login

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ const User = require('../models/User');
 const JWTSECRET = "generalkenobi"
 
 //creating user
-router.post('/', [
+router.post('/createuser', [
     //validating
     body('email', 'Enter a valid email').isEmail(),
     body('name', 'Name cannot be this short').isLength({ min: 3 }),
@@ -48,8 +48,49 @@ router.post('/', [
     } catch (e) {
         res.status(500).send("Something went wrong")
     }
-
-
 })
+
+//authenticating a user - login
+
+router.post('/login', [
+        //validating
+        body('email', 'Enter a valid email').isEmail(),
+        body('password', 'Password cannot be blank').exists()
+    ],
+
+    async(req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email })
+            if (!user) {
+                return res.status(400).json({ error: "Incorrect username or password" })
+            }
+
+            const passtocompare = await bcrypt.compare(password, user.password);
+
+            if (!passtocompare) {
+                return res.status(400).json({ error: "Incorrect username or password" })
+            }
+
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            const authToken = jwt.sign(data, JWTSECRET)
+            res.send(authToken)
+
+        } catch (e) {
+            res.status(500).send("Something went wrong")
+        }
+
+    })
 
 module.exports = router
